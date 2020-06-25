b9e4faf9fa4bf05a8300201e0f89b1d9a106f327
Abhideep Maity <abhideepm@gmail.com>
2020-06-25 13:45:16 +0530

    landing page

--- landingpage.js
+++ landingpage.js
@@ -1,2 +1,22 @@
 var main = document.createElement('div')
+main.classList.add(
+	'margin-center',
+	'd-flex',
+	'flex-column',
+	'align-items-center',
+	'text-center'
+)
 document.body.appendChild(main)
+
+var title = document.createElement('h1')
+title.innerHTML = 'Welcome to Sudoku!'
+main.appendChild(title)
+
+var subtitle = document.createElement('h3')
+subtitle.innerHTML = 'Click the button below to Play!'
+main.appendChild(subtitle)
+
+var playbtn = document.createElement('button')
+playbtn.classList.add('btn', 'btn-success')
+playbtn.innerHTML = 'Play!'
+main.appendChild(playbtn)
