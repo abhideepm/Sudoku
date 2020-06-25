fd4849d176f3b4463eefb65ddbc95cf547178cce
Abhideep Maity <abhideepm@gmail.com>
2020-06-25 14:08:50 +0530

    landing page done

--- landingpage.js
+++ landingpage.js
@@ -8,15 +8,20 @@ main.classList.add(
 )
 document.body.appendChild(main)
 
-var title = document.createElement('h1')
+var title = document.createElement('div')
+title.classList.add('my-3', 'display-3', 'text-white')
 title.innerHTML = 'Welcome to Sudoku!'
 main.appendChild(title)
 
-var subtitle = document.createElement('h3')
+var subtitle = document.createElement('div')
+subtitle.classList.add('my-3', 'text-white', 'h3')
 subtitle.innerHTML = 'Click the button below to Play!'
 main.appendChild(subtitle)
 
-var playbtn = document.createElement('button')
-playbtn.classList.add('btn', 'btn-success')
+var playbtn = document.createElement('a')
+playbtn.classList.add('my-3')
+playbtn.classList.add('btn', 'btn-outline-light', 'btn-lg')
+playbtn.href = 'sudokugame.html'
+playbtn.setAttribute('role', 'button')
 playbtn.innerHTML = 'Play!'
 main.appendChild(playbtn)
